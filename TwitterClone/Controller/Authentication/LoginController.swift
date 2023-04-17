caa937ccac9975b5abb1228327204be7a41264ae
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-04-17 18:03:15 -0300

    login screen layout

--- TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/Controller/Authentication/LoginController.swift
@@ -19,6 +19,51 @@ class LoginController: UIViewController {
         return iv
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = Utilities.inputContainerView(withImage: #imageLiteral(resourceName: "ic_mail_outline_white_2x-1"), textField: emailTextField)
+        
+        return view
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = Utilities.inputContainerView(withImage: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tf = Utilities.textField(withPlaceholder: "Email")
+        
+        return tf
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tf = Utilities.textField(withPlaceholder: "Password")
+        tf.isSecureTextEntry = true
+        
+        return tf
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.twitterBlue, for: .normal)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalToConstant : 50).isActive = true
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var dontHaveAccountButton: UIButton = {
+        let button = Utilities.attributedButton("Don't have an account?", " Sign Up")
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        
+        return button
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -27,6 +72,14 @@ class LoginController: UIViewController {
     }
     
     // MARK: - Selectors
+    
+    @objc func handleLogin() {
+        print("login clicked")
+    }
+    
+    @objc func handleShowSignUp() {
+        print("ShowSignUp clicked")
+    }
     
     // MARK: - Helpers
     
@@ -37,5 +90,16 @@ class LoginController: UIViewController {
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
         logoImageView.setDimensions(width: 150, height: 150)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView, loginButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 40, paddingRight: 40)
     }
 }
