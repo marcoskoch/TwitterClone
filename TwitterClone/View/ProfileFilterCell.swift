e771f2a7f1cd5693e4f5adf9346ff99797ac393c
Marcos Michel Machado Koch <marcos_koch@hotmail.com>
2023-05-09 08:45:46 -0300

    add filter bar

---
+++ TwitterClone/View/ProfileFilterCell.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileFilterCell.swift
+//  TwitterClone
+//
+//  Created by Marcos Michel on 08/05/23.
+//
+
+import UIKit
+
+class ProfileFilterCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Test Filter"
+        return label
+    }()
+    
+    override var isSelected: Bool {
+        didSet {
+            titleLabel.font = isSelected ? UIFont.boldSystemFont(ofSize: 16) : UIFont.systemFont(ofSize: 14)
+            titleLabel.textColor = isSelected ? .twitterBlue : .lightGray
+        }
+    }
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        
+        addSubview(titleLabel)
+        titleLabel.center(inView: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Properties
+}
